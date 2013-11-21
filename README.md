f7f526490dc1e4cc6dc0f8cf3552152ecdeb0b11
Joe Franco <joe@ini.usc.edu>
2013-11-20 16:25:41 -0800

    moved the exception handler to a much more appropriate location. added note in interactive script regarding execution in multi-version environments. Rewrote the README as well

--- README.md
+++ README.md
@@ -1,10 +1,19 @@
-Simple homedir creation script written in Python.
+==Homedir creation script==
+This python script is meant for use on linux operating systems. You can get it to run on OSX, but I haven't seen any real reason to use it there yet, so I haven't tested that use.
 
-This script assumes that you have sufficient privs to sudo on the box you use to create the directories. 
-Usage: chmod +x a.py then execute with one argument:
-sudo ./a.py username
-You can also copy this file, sans extension, into your local bin dir that isn't /usr/bin and run it by name, assuming you have that directory in your path. Personally I would put it in /usr/local/sbin or similar. 
+===homedir.py===
+This is the initial script that kicked this off. I'm no longer working on this one, so I'll likely remove it soon.
 
-homedir_commented.py is just a heavily commented version of the same script. It exists solely for reference, as the other version is cleaner for real world use.
+===homedir_commented.py===
+This is a commented version of the original script. Like above, this is not really necessary anymore and will be removed soon.
 
-The interactive version of this script is interactive_homedir.py. This should be a bit more user-friendly and it even thanks you when you end the program. To execute, chmod +x and sudo ./interactive_homedir.py (or whatever you end up calling it). Script does not require or parse arguments.
+===interactive_homedir.py===
+This is where I am spending the bulk of my time now. This script assumes you are running a pre-3.x version of python because I haven't tested it in 3.x. Give it a shot if you like and feel free to port anything that doesn't work fluidly between the versions.
+
+===usage of interactive_homedir.py===
+Run it, follow directions. There is a comment in the header of the file that talks about running this with +x props via
+<pre>$ ./interactive_homedir.py</pre>
+
+but if you want to operate in a stricter environment, erase/comment out the env line (line 1) and run it with your preferred version of python:
+<pre>$ /usr/local/python-2.7.3/bin/python interactive_homedir.py</pre>
+
--- interactive_homedir.py
+++ interactive_homedir.py
@@ -1,48 +1,50 @@
 #!/usr/bin/env python
+# be careful with this. On systems with multiple environments, you should either change
+# this line to the specific path of your pre-3.x version of python, or erase that line 
+# and run this as 'sudo /path/to/python/2.7/or/earlier <this command>
 
 import pwd
 import sys
 import string
 import os
 
-
-
 def find_and_make():
-        user = raw_input("Please enter the username whose homedir you wish to create:")
-        try:
-                pwd.getpwnam(user)
-                org_pwnam = pwd.getpwnam(user)
-                print org_pwnam[0], "should have a home directory at", org_pwnam[5], "and a group numeral of", org_pwnam[3]
-                org_pathdir = org_pwnam[5]
-                print "Checking path:", org_pathdir
+   user = raw_input("Please enter the username whose homedir you wish to create:")
+   try:
+      pwd.getpwnam(user)
+   except KeyError:
+      print "User not found."
+      keep_going()
 
-                if os.path.exists(org_pathdir):
-                        print "Home directory already exists."
-                        keep_going()
-                else:
-                        print "Directory does not exist. Creating and applying appropriate ownership"
-                        os.makedirs(org_pathdir)
-                        os.chown(org_pathdir, org_pwnam[2], org_pwnam[3])
-                        keep_going()
-        except KeyError:
-                pass
-                print "User not found."
-                keep_going()
+   org_pwnam = pwd.getpwnam(user)
+   print org_pwnam[0], "should have a home directory at", org_pwnam[5], "and a group numeral of", org_pwnam[3]
+   org_pathdir = org_pwnam[5]
+   print "Checking path:", org_pathdir
+
+   if os.path.exists(org_pathdir):
+      print "Home directory already exists."
+      keep_going()
+   else:
+      print "Directory does not exist. Creating and applying appropriate ownership"
+      os.makedirs(org_pathdir)
+      os.chown(org_pathdir, org_pwnam[2], org_pwnam[3])
+      keep_going()
 
 def keep_going():
-        yes = set(['yes', 'y', 'Y', 'Yes', 'ye', ''])
-        no = set(['no', 'No', 'n', 'N'])
-        more_answer = raw_input("Would you like to create another home directory?")
-        if more_answer in yes:
-                find_and_make()
-        elif more_answer in no:
-                print "Done."
-        else:
-                print "Please say yes or no"
-                keep_going()
+   yes = set(['yes', 'y', 'Y', 'Yes', 'ye', ''])
+   no = set(['no', 'No', 'n', 'N'])
+   more_answer = raw_input("Would you like to create another home directory? [yes]")
+   if more_answer in yes:
+      find_and_make()
+   elif more_answer in no:
+      print "Done."
+      sys.exit()
+   else:
+      print "Please say yes or no"
+      keep_going()
 
 
 if os.getuid() is not 0:
-        print "rerun command with sudo"
+   print "rerun command with sudo"
 else:
-        find_and_make()
+   find_and_make()
